d2398f1f5072793788764234c3662948ca79286e
khanhbui29 <kbui2985@gmail.com>
2017-05-19 14:33:28 -0400

    Update IndexController.java

--- src/main/java/com/kb/demo/web/IndexController.java
+++ src/main/java/com/kb/demo/web/IndexController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.demo.kb.service.HelloWorldService;
 
 @Controller
 public class IndexController {
